558646676f2774ab82e9d6bd38db767de9dab659
Jeffrey Spurling <89798840+JeffySpur@users.noreply.github.com>
2021-11-22 10:53:06 -0500

    Add project files.

---
+++ CoffeeConnect.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoffeeConnect.WebMVC.Startup))]
+namespace CoffeeConnect.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
